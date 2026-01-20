2a2fa830b52e0a5843fc80642180345ecf4b3311
Asutosh Mohanty <asutoshmohanty181@gmail.com>
2026-01-20 11:27:07 +0530

    Update server URL to production endpoint

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { Children, createContext } from 'react'
 export const authDataContext = createContext()
 
 function AuthContext({children}) {
-    const serverUrl = "http://localhost:8000"
+    const serverUrl = "https://linkdnproject.onrender.com"
     let value = {
         serverUrl
     }
